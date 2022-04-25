1b472419d3f348c85f0904e5aa69d4e667899a6b
CharlesMuvaka12345 <charles.uvaka@student.moringaschool.com>
2022-04-25 16:27:59 +0300

    add a test to check for the userInputMessage

--- src/test/java/org/example/EncryptTest.java
+++ src/test/java/org/example/EncryptTest.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EncryptTest {
+    Encrypt encryption = new Encrypt("Charles Muvaka", 3);
+
+    @Test
+    public void getUserInput_checksIfThereIsUserInput() {
+        assertEquals("Charles Muvaka", encryption.getmUserMessage());
+    }
 
 }

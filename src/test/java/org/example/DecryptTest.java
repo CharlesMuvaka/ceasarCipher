ad7cdae50257f262b565a3f307f40528da1f8a6f
CharlesMuvaka12345 <charles.uvaka@student.moringaschool.com>
2022-04-25 16:22:15 +0300

    create a decription test class

--- src/test/java/org/example/DecryptTest.java
+++ src/test/java/org/example/DecryptTest.java
@@ -1,6 +1,16 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class DecryptTest {
-  
+
+    Decrypt decryption = new Decrypt("Charles Muvaka", 3);
+
+    @Test
+    public void getUserInput_checksIfThereIsUserInput() {
+        assertEquals("Charles Muvaka", decryption.getmUserMessage());
+    }
+
 }

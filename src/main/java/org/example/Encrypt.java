74f523be27787167910a2e287c5a563845b05534
CharlesMuvaka12345 <charles.uvaka@student.moringaschool.com>
2022-04-25 16:27:16 +0300

    add getters and setter and a constructor

--- src/main/java/org/example/Encrypt.java
+++ src/main/java/org/example/Encrypt.java
@@ -1,4 +1,27 @@
 package org.example;
 
 public class Encrypt {
+    private int mKey;
+    private String mUserMessage;
+    public Encrypt(String userMessage, int key) {
+        this.mUserMessage = userMessage;
+        this.mKey = key;
+
+    }
+
+    public int getmKey() {
+        return mKey;
+    }
+
+    public void setmKey(int mKey) {
+        this.mKey = mKey;
+    }
+
+    public String getmUserMessage() {
+        return mUserMessage;
+    }
+
+    public void setmUserMessage(String mUserMessage) {
+        this.mUserMessage = mUserMessage;
+    }
 }

bb850b60ae8512f3a80bb6669f496da272adffbf
CharlesMuvaka12345 <charles.uvaka@student.moringaschool.com>
2022-04-25 16:45:26 +0300

    add a method to decrypt users message

--- src/main/java/org/example/Decrypt.java
+++ src/main/java/org/example/Decrypt.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Locale;
+
 public class Decrypt {
 
     private String mUserMessage;
@@ -25,4 +27,34 @@ public class Decrypt {
     public void setmUserKey(int mUserKey) {
         this.mUserKey = mUserKey;
     }
+
+    public String encryptUserMessage(){
+
+        String myAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuilder userDecryptedMessage = new StringBuilder();
+
+
+        String userMessageToBeDecrypted = getmUserMessage().toLowerCase();
+        char[] userMessageArray = userMessageToBeDecrypted.toCharArray();
+        int userEncryptionKey = getmUserKey();
+
+        if (userEncryptionKey>0 && userEncryptionKey<26){
+            for (char c:userMessageArray
+            ) {
+                if (Character.isLetter(c)){
+                    int currentPosition = myAlphabet.indexOf(c);
+                    int newIndex = (currentPosition + userEncryptionKey) % 26;
+                    char encryptedLetter = myAlphabet.charAt(newIndex);
+                    userDecryptedMessage.append(encryptedLetter);
+                }else{
+                    userDecryptedMessage.append(c);
+                }
+
+            }
+
+        }
+
+        return userDecryptedMessage.toString();
+    }
+
 }
